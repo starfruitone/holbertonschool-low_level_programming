7d0cedc90274a1a610836f09afa70bc0f4122fa6
starfruitone <starfruitworld@gmail.com>
2025-10-26 00:55:25 +0100

    Add files via upload

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _abs- aide pour la conduite pour le systeme de frein
+* @a: le a initie les chiffres
+* Return: Always 0 (Success)
+*/
+
+#include "main.h"
+
+int _abs(int a)
+{
+	if (a > 0)
+	{
+	return (a);
+	}
+	else if (a < 0)
+	{
+	return (-a);
+	}
+return (0);
+}
