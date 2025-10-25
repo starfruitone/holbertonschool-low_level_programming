cdc548f8c3980e206133d2e4dc9acf982f1c2c92
starfruitone <duranta.morgan@gmail.com>
2025-10-25 22:16:52 +0000

    Betty style fix for task 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,12 +2,12 @@
 
 int main(void)
 {
-    int n = 1;
-    if (n > 0)
-        printf("0 is positive\n");
-    else if (n == 0)
-        printf("0 is zero\n");
-    else
-        printf("0 is negative\n");
-    return (0);
+	int n = 1;
+	if (n > 0)
+		printf("0 is positive\n");
+	else if (n == 0)
+		printf("0 is zero\n");
+	else
+		printf("0 is negative\n");
+	return (0);
 }
