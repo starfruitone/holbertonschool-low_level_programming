8639a6b4a7b625eabc65763b1b3e2d805d20cc9e
starfruitone <duranta.morgan@gmail.com>
2025-10-25 21:28:51 +0000

    Completed task 0

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n = 1;
+    if (n > 0)
+        printf("0 is positive\n");
+    else if (n == 0)
+        printf("0 is zero\n");
+    else
+        printf("0 is negative\n");
+    return (0);
+}
