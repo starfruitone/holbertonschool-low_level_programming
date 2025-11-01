2200f0c174049e039f7111e59f078b77fd94e7b8
starfruitone <duranta.morgan@gmail.com>
2025-11-01 16:28:52 +0000

    most nums

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Prints numbers 0 to 9 except 2 and 4, followed by newline
+ * print_most_numbers - Prints numbers 0-9 except 2 and 4
  *
  * Return: void
  */
+
 void print_most_numbers(void)
 {
 	char str[] = "01356789\n";
