72aa2303cd5abeb1d9939a314b7285d6d3f971e3
starfruitone <duranta.morgan@gmail.com>
2025-11-01 18:14:14 +0000

    3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string followed by a new line to stdout
+ * @str: Pointer to the string to print
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
