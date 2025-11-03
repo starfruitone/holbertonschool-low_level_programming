72cf7cbb56236deee020e84ca875430d2fe17bf6
starfruitone <duranta.morgan@gmail.com>
2025-11-03 09:40:37 +0000

    Add _strncpy for task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies a string up to n bytes
+ * @dest: Destination string
+ * @src: Source string
+ * @n: Number of bytes to copy from src
+ *
+ * Return: Pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
