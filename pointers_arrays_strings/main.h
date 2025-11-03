1b2d19c9e4def80ac757ea2560ce6213ae06ed5d
starfruitone <duranta.morgan@gmail.com>
2025-11-03 10:47:41 +0000

    task 0 for week 5

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -14,7 +14,7 @@ void puts_half(char *str);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 */
-char *_strcat(char *dest, char *src);
+/*char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
@@ -22,4 +22,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *s);
+*/
+char *_memset(char *s, char b, unsigned int n);
+
 #endif
