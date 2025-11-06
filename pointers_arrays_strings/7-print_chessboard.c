6d419c45e5c87820f45fa0461cab6f7f25470c02
starfruitone <duranta.morgan@gmail.com>
2025-11-06 09:48:47 +0000

    print_chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,21 +1,15 @@
 #include "main.h"
-
 /**
  * print_chessboard - Prints the chessboard
- * @a: 2D array representing the chessboard
- *
- * Return: void
+ * @a: 2D array 8x8
  */
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
-
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-		{
 			_putchar(a[i][j]);
-		}
 		_putchar('\n');
 	}
 }
