0546c7504e559cebc812ee1c028ab505a01edd1e
starfruitone <duranta.morgan@gmail.com>
2025-11-07 14:42:16 +0000

    ur heisenberg

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints its name
+ * @argc: Argument count (unused)
+ * @argv: Argument vector (argv[0] is program name)
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+pro tip kids, dont put the repo inside the wrong one, you might end up going crazy
