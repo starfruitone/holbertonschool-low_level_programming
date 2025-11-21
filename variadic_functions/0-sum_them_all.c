31e367af2cbcb2c3366f974d37f312db00a90e65
starfruitone <duranta.morgan@gmail.com>
2025-11-21 16:27:17 +0000

    varaiadik stuff and sums i guess

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Adds together all arguments
+ * @n: Number of parameters to sum
+ * Return: Total sum, or 0 if n == 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	int total = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		total += va_arg(ap, int);
+
+	va_end(ap);
+
+	return (total);
+}
---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
+/* Sums all parameters passed to the function */
+int sum_them_all(const unsigned int n, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
