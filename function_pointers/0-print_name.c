74b7d49b8164a38ed25bbf924d865346d231ec01
starfruitone <duranta.morgan@gmail.com>
2025-11-21 11:10:50 +0000

    fix

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
- * print_name - Prints a name using a function pointer
+ * print_name - Prints a name using the provided function pointer
  * @name: The name to print
  * @f: Pointer to a function that prints a name
- *
  */
 void print_name(char *name, void (*f)(char *))
 {
