0d8af0ae5f70d4024785de8aec837580512c3eca
starfruitone <duranta.morgan@gmail.com>
2025-12-05 17:19:42 +0000

    feat: created 1-list_len/c

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - counts the elements in a list_t list
+ * @h: pointer to the first node of the list
+ *
+ * Return: number of elements in the list
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *node = h;
+	size_t size = 0;
+
+	while (node != NULL)
+	{
+		size++;
+		node = node->next;
+	}
+
+	return (size);
+}
