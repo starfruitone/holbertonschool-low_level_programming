6cfccc7875616b62825f8d75f2c0bd58172b8023
starfruitone <duranta.morgan@gmail.com>
2025-12-05 17:15:24 +0000

    fix: bugs in 0-print_list.c and lists.h

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,24 +2,26 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elemments of a list_t list
- * @h: pointer to the head of the list
- * return: number of nodes
+ * print_list - prints each element in a list_t list
+ * @h: pointer to the first node of the list
+ *
+ * Return: number of nodes in the list
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+	const list_t *node = h;
+	size_t total = 0;
 
-    while (h)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
+	while (node != NULL)
+	{
+		if (node->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", node->len, node->str);
 
-        h = h->next;
-        count++;
-    }
+		node = node->next;
+		total++;
+	}
 
-    return (count);
+	return (total);
 }
