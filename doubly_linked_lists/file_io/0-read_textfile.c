4b97aa5107f091294466cf52fee29c8cdc773ad3
starfruitone <duranta.morgan@gmail.com>
2025-12-06 15:28:45 +0000

    fix: readtextfile.c

--- doubly_linked_lists/file_io/0-read_textfile.c
+++ doubly_linked_lists/file_io/0-read_textfile.c
@@ -1,51 +1,47 @@
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to stdout
- * @filename: name of the file to read
+ * read_textfile - reads a text file and prints it to the stdout
+ * @filename: file to read
  * @letters: number of letters to read and print
- * Return: actual number of letters read and printed, 0 on failure
+ * Return: actual number of letters read and printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t bytes_read, bytes_written;
-	char *buffer;
+	int fd, bytes_read, bytes_written;
+	char *buff;
 
 	if (!filename)
 		return (0);
 
+	buff = malloc(letters);
+	if (!buff)
+		return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);
-
-	buffer = malloc(sizeof(char) * letters);
-	if (!buffer)
 	{
-		close(fd);
+		free(buff);
 		return (0);
 	}
 
-	bytes_read = read(fd, buffer, letters);
+	bytes_read = read(fd, buff, letters);
 	if (bytes_read == -1)
 	{
-		free(buffer);
 		close(fd);
+		free(buff);
 		return (0);
 	}
 
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	bytes_written = write(STDOUT_FILENO, buff, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
-		free(buffer);
 		close(fd);
+		free(buff);
 		return (0);
 	}
 
-	free(buffer);
 	close(fd);
-	return (bytes_written);
+	free(buff);
+	return (bytes_read);
 }
