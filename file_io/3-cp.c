13aaac73c52c86aed8e11bea850f1ca544383786
starfruitone <duranta.morgan@gmail.com>
2025-12-07 19:17:14 +0000

    fix: cp program

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,76 +1,71 @@
 #include "main.h"
 
-/**
- * print_error - print error message and exit with the given code
- * @code: exit status
- * @file: filename (for 98/99) or NULL
- * @fd: file descriptor (for 100) or -1
- */
-static void print_error(int code, const char *file, int fd)
+static void handle_err(int code, const char *file, int fd)
 {
 	if (code == 97)
+	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	}
 	else if (code == 98)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+	}
 	else if (code == 99)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+	}
 	else if (code == 100)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	}
 	exit(code);
 }
 
-/**
- * main - copies the content of a file to another file
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: 0 on success, or exits with 97/98/99/100 on error
- */
 int main(int argc, char **argv)
 {
-	int fd_from, fd_to, c;
-	ssize_t r, w;
-	char buf[1024];
+	int src_fd, dst_fd, status;
+	ssize_t n_read, n_written;
+	char buffer[1024];
 
 	if (argc != 3)
-		print_error(97, NULL, -1);
+		handle_err(97, NULL, -1);
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		print_error(98, argv[1], -1);
+	src_fd = open(argv[1], O_RDONLY);
+	if (src_fd == -1)
+		handle_err(98, argv[1], -1);
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
+	dst_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (dst_fd == -1)
 	{
-		close(fd_from);
-		print_error(99, argv[2], -1);
+		close(src_fd);
+		handle_err(99, argv[2], -1);
 	}
 
-	while ((r = read(fd_from, buf, 1024)) > 0)
+	while ((n_read = read(src_fd, buffer, 1024)) > 0)
 	{
-		w = write(fd_to, buf, r);
-		if (w == -1 || w != r)
+		n_written = write(dst_fd, buffer, n_read);
+		if (n_written == -1 || n_written != n_read)
 		{
-			close(fd_from);
-			close(fd_to);
-			print_error(99, argv[2], -1);
+			close(src_fd);
+			close(dst_fd);
+			handle_err(99, argv[2], -1);
 		}
 	}
 
-	if (r == -1)
+	if (n_read == -1)
 	{
-		close(fd_from);
-		close(fd_to);
-		print_error(98, argv[1], -1);
+		close(src_fd);
+		close(dst_fd);
+		handle_err(98, argv[1], -1);
 	}
 
-	c = close(fd_from);
-	if (c == -1)
-		print_error(100, NULL, fd_from);
+	status = close(src_fd);
+	if (status == -1)
+		handle_err(100, NULL, src_fd);
 
-	c = close(fd_to);
-	if (c == -1)
-		print_error(100, NULL, fd_to);
+	status = close(dst_fd);
+	if (status == -1)
+		handle_err(100, NULL, dst_fd);
 
 	return (0);
 }
