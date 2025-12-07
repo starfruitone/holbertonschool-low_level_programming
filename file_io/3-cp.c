75207addc1d71b4427a53d76644b9e7e2c36cb4b
starfruitone <duranta.morgan@gmail.com>
2025-12-07 19:14:36 +0000

    fix: cp program

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,70 +1,76 @@
 #include "main.h"
 
 /**
- * main - copies the content of a file to another file
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 on success, or exits with error codes 97, 98, 99, 100
+ * print_error - print error message and exit with the given code
+ * @code: exit status
+ * @file: filename (for 98/99) or NULL
+ * @fd: file descriptor (for 100) or -1
  */
-static void fail(int code, char *file, int fd)
+static void print_error(int code, const char *file, int fd)
 {
 	if (code == 97)
-	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-	}
 	else if (code == 98)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
-	}
 	else if (code == 99)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
-	}
 	else if (code == 100)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-	}
 	exit(code);
 }
 
+/**
+ * main - copies the content of a file to another file
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, or exits with 97/98/99/100 on error
+ */
 int main(int argc, char **argv)
 {
-	int fd_from, fd_to, c_status;
-	ssize_t bytes_read = 1, bytes_written;
+	int fd_from, fd_to, c;
+	ssize_t r, w;
 	char buf[1024];
 
 	if (argc != 3)
-		fail(97, NULL, 0);
+		print_error(97, NULL, -1);
 
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-		fail(98, argv[1], 0);
+		print_error(98, argv[1], -1);
 
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		close(fd_from);
-		fail(99, argv[2], 0);
+		print_error(99, argv[2], -1);
 	}
 
-	while (bytes_read)
+	while ((r = read(fd_from, buf, 1024)) > 0)
 	{
-		bytes_read = read(fd_from, buf, 1024);
-		if (bytes_read == -1)
-			fail(98, argv[1], 0);
-
-		bytes_written = write(fd_to, buf, bytes_read);
-		if (bytes_written == -1)
-			fail(99, argv[2], 0);
+		w = write(fd_to, buf, r);
+		if (w == -1 || w != r)
+		{
+			close(fd_from);
+			close(fd_to);
+			print_error(99, argv[2], -1);
+		}
 	}
 
-	c_status = close(fd_from);
-	if (c_status == -1)
-		fail(100, NULL, fd_from);
+	if (r == -1)
+	{
+		close(fd_from);
+		close(fd_to);
+		print_error(98, argv[1], -1);
+	}
 
-	c_status = close(fd_to);
-	if (c_status == -1)
-		fail(100, NULL, fd_to);
+	c = close(fd_from);
+	if (c == -1)
+		print_error(100, NULL, fd_from);
+
+	c = close(fd_to);
+	if (c == -1)
+		print_error(100, NULL, fd_to);
 
 	return (0);
 }
